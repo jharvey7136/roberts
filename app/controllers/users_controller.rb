8dae19d0bc651b3fd2d49a18eb0f59c03e7197b5
jharvey7136 <jharvey7136@mail.kvcc.edu>
2018-01-30 17:08:45 -0500

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    @user = User.find(params[:id])
+
   end
 
   # GET /users/new
@@ -28,6 +30,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        flash[:success] = "Welcome to Roberts Enterprise!"
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
