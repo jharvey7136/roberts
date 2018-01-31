7d246a58a22c1ba39ef8ef486323327f689567c0
jharvey7136 <jharvey7136@mail.kvcc.edu>
2018-01-30 22:17:25 -0500

    Implement basic login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,5 @@
+ENV['RAILS_ENV'] ||= 'test'
+
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
@@ -7,4 +9,8 @@ class ActiveSupport::TestCase
   #include ApplicationHelper
 
   # Add more helper methods to be used by all tests here...
+  # Returns true if a test user is logged in.
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
