7be3edd2f65f2a6b1a992a8c0f7777dc6c1879a5
jharvey7136 <jharvey7136@mail.kvcc.edu>
2018-04-03 21:36:58 -0400

    verify admin for blog posting, blog fixes

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,13 +1,21 @@
 class Album < ApplicationRecord
+
+
   validates_presence_of :title
+
   belongs_to :topic
+
+
 
   scope :campground_album_items, -> {where(topic_id: '1')}
   scope :river_run_album_items, -> {where(topic_id: '2')}
   scope :estate_park_album_items, -> {where(topic_id: '3')}
 
-  mount_uploader :thumb_image, AlbumUploader
-  mount_uploader :main_image, AlbumUploader
+
+
+
+  mount_uploader :image, ImageUploader
+
 
   def self.by_position
     order("position ASC")
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,8 @@ class Post < ApplicationRecord
   belongs_to :category
   validates :title, :content, :category_id, presence: true
 
+
+
   mount_uploader :image, ImageUploader
 
 
