cae7af398b79340cc2caf2004fd81a3ab6a8dc18
jharvey7136 <jharvey7136@mail.kvcc.edu>
2018-04-11 22:54:03 -0400

    precompiled for prod

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -21,7 +21,10 @@ Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 Rails.application.config.assets.precompile += %w( side-nav.css )
 Rails.application.config.assets.precompile += %w( side-nav.js )
 
-
+Rails.application.config.assets.precompile += %w( skrollr.css )
+Rails.application.config.assets.precompile += %w( skrollr/imagesloaded.js )
+Rails.application.config.assets.precompile += %w( skrollr/skrollr.js )
+Rails.application.config.assets.precompile += %w( skrollr/_main.js )
 
 
 
