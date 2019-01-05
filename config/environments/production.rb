110bafbde0976f1e79df5900448ab248b096ede0
jharvey7136 <johnharvey33@live.com>
2019-01-05 17:41:46 -0500

    disable assets compile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
   config.assets.digest = true
   config.serve_static_assets = true
 
